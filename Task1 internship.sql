f0ec48d87393405f617ddeb046efa026a2bb48d1
Manoj <113962196+Manoj1412@users.noreply.github.com>
2025-06-23 19:08:12 +0530

    Task1 internship.sql

--- Task1 internship.sql
+++ Task1 internship.sql
@@ -1,17 +1,18 @@
 CREATE DATABASE office;
 use office;
 CREATE TABLE department(
-d_id int,
-dept_name VARCHAR(20),
+d_id int PRIMARY KEY,
+dept_name VARCHAR(20) NOT NULL,
 emp_count int);
 INSERT INTO department VALUES(1,"Sales",150),(2,"Production",300),(3,"Editing",30),(4,"Customer support",50);
 SELECT * FROM department;
 CREATE TABLE Employees(
-e_id int,
+e_id int PRIMARY KEY,
 e_name VARCHAR(20),
 gender VARCHAR(6),
-age int,
-Tablesdept_id int);
+age int NOT NULL,
+dept_id INT,
+FOREIGN KEY(dept_id) REFERENCES department(d_id));
 INSERT INTO Employees VALUES
 (1, 'Alice', 'Female', 30, 1),
 (2, 'Bob', 'Male', 25, 1),
